10ad8e2afc9d2b7842c2c560545fb5ec01b20d34
javik223 <javik@live.com>
2019-11-27 06:55:02 +0100

    refresh

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,21 @@ const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  // mode: 'production',
+  // mode: 'development',
+  mode: 'production',
   entry: __dirname + '/src/js/app.js',
   output: {
     path: __dirname + '/dist/assets/js',
     filename: 'app.js',
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
       },
-    ],
+    }, ],
   },
   // plugins: [new HtmlWebpackPlugin({ template: './src/**/*.pug' })],
 };
